c29dcb4a1302b7a36873515528bc3898eb4fdb5c
TencerDe <tsharma2907@gmail.com>
2026-02-10 16:43:45 +0530

    admin panel added

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,3 +1,22 @@
 from django.contrib import admin
+from .forms import CustomUserCreationForm, CustomUserChangeForm
+from .models import CustomUser
+from django.contrib.auth.admin import UserAdmin
 
 # Register your models here.
+
+
+
+class CustomUserAdmin(UserAdmin):
+    add_form = CustomUserCreationForm
+    change_form = CustomUserChangeForm
+    model = CustomUser
+    list_display = [
+        "email"]
+    
+    search_fields = ['email']
+    ordering = ('-date_joined',)
+
+
+admin.site.register(CustomUser, CustomUserAdmin)
+
