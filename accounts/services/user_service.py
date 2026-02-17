2a9845a1b43aecb6a5399aa791cb55cb2b185207
TencerDe <tsharma2907@gmail.com>
2026-02-17 16:52:34 +0530

    user service bug resolved where the user password and user creation field were colliding and throwing error

--- accounts/services/user_service.py
+++ accounts/services/user_service.py
@@ -1,16 +1,13 @@
 from django.contrib.auth.base_user import BaseUserManager
+from accounts.models import CustomUser
 
+def create_user_for_signup(email, password, **extra_fields):
+    extra_fields.setdefault("is_verified", False)
 
-class signup():
-    def create_user_for_signup(self, email, password, **extra_fields):
-        if not email:
-            raise ValueError('Please enter a email address')
-        email = self.normalize_email(email)
-        user = self.model(email = email, **extra_fields)
-        user.set_password(password)
-        user.save(using=self.db)
-        user.is_verified = False
-        return user
+    user = CustomUser.objects.create_user(
+        email=email,
+        password=password,
+        **extra_fields
+    )
 
-
-
+    return user
