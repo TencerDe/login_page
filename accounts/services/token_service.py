b81af6f517bfbd2b8a14a28acbbe2adc907a757c
TencerDe <tsharma2907@gmail.com>
2026-02-18 22:20:28 +0530

    Fixeed some bugs and make the code more production oriented thanks to CPT

--- accounts/services/token_service.py
+++ accounts/services/token_service.py
@@ -4,19 +4,28 @@ from datetime import timedelta
 from accounts.models import EmailVerificationToken
 
 
-def creating_token():
-    token_32_bytes = secrets.token_urlsafe(32)
-    print(f"32-bytetoken:{token_32_bytes}")
-    return creating_token()
-
-def token_expiry(request):
+def create_verification_token(user):
+    EmailVerificationToken.objects.filter(
+        user=user, is_used = False).delete()
+    token=  secrets.token_urlsafe(32)
     expiry_time = timezone.now() + timedelta(minutes=15)
-    request.session.set_expiry(expiry_time)
+    EmailVerificationToken.objects.create(
+        user = user, token = token, expires_at = expiry_time)
+    return token
 
 
-token_expiry.objects.filter(expires_lt = timezone.now()).delete()
+def validate_verification_token(token):
+    token_obj = EmailVerificationToken.objects.filter(
+        token = token, is_used = False).first()
+    if not token_obj:
+        return None
+    if token_obj.expires_at < timezone.now():
+        return None
+    return token_obj
 
-#def delete_token():
 
+def mark_token_used(token_obj):
+    token_obj.is_used = True
+    token_obj.save()
 
 
