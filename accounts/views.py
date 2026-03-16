060b6a810985537ae8f40ac9cc5874d29d5789af
TencerDe <tsharma2907@gmail.com>
2026-03-16 23:24:33 +0530

    update project structure and apps which were untracked

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,37 @@
-from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
+
+from .serializers import RegisterSerializer
+
+from accounts.services.user_service import create_user_for_signup
+from accounts.services.token_service import create_verification_token
+from accounts.services.email_service import send_verification_email
+
 
 # Create your views here.
+
+
+class SignupView(APIView):
+
+    def post(self, request):
+        serializer = RegisterSerializer(data=request.data)
+
+        if not serializer.is_valid():
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+        email = serializer.validated_data["email"]
+        password = serializer.validated_data["password"]
+
+
+        user = create_user_for_signup(email=email, password=password)
+        token = create_verification_token(user)
+        send_verification_email(user, token)
+
+        return Response(
+            {
+                "message": "Signup successful. Please verify your email."
+            },
+            status=status.HTTP_201_CREATED
+        )
+
